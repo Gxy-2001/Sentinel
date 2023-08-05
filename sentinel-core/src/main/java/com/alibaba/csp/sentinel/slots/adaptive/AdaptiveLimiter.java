2eb6e75657310b52057173105c1c4448cc7f1c84
Guan <1075586877@qq.com>
2023-08-05 17:36:14 +0800

    feature: 资源自适应限流的初步实现
    Bump version to 2.0.0-alpha2-SNAPSHOT
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

---
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/adaptive/AdaptiveLimiter.java
@@ -0,0 +1,29 @@
+package com.alibaba.csp.sentinel.slots.adaptive;
+
+import com.alibaba.csp.sentinel.node.ClusterNode;
+import com.alibaba.csp.sentinel.slots.clusterbuilder.ClusterBuilderSlot;
+import com.alibaba.csp.sentinel.slots.statistic.StatisticSlot;
+
+import java.util.Queue;
+
+/**
+ * @author ElonTusk
+ * @name AdaptiveLimiter
+ * @date 2023/8/7 14:14
+ */
+public class AdaptiveLimiter {
+    public static void adaptiveLimit(AdaptiveRule rule) {
+        ClusterNode node = ClusterBuilderSlot.getClusterNode(rule.getResource());
+        if (node == null) return;
+
+        double minRt = node.minRt();
+        double rt = node.avgRt();
+        double passQps = node.passQps();
+        Queue<Integer> oldCounts = rule.getOldCounts();
+        int newLimit = rule.getLimiter().update(oldCounts, minRt, rt, passQps);
+        rule.setCount(newLimit);
+        System.out.println("设置新阈值" + newLimit);
+        rule.addCount(newLimit);
+        rule.setTimes(0);
+    }
+}
---
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/adaptive/algorithm/VegasLimit.java
@@ -0,0 +1,48 @@
+package com.alibaba.csp.sentinel.slots.adaptive.algorithm;
+
+import java.util.Queue;
+
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+
+/**
+ * @author ElonTusk
+ * @name Vegas
+ * @date 2023/8/2 14:47
+ */
+public class VegasLimit extends AbstractLimit {
+
+    @Override
+    public int update(Queue<Integer> oldLimits, double minRt, double rt, double passQps) {
+        double estimatedLimit = 20;
+        for (Integer oldLimit : oldLimits) {
+            estimatedLimit = oldLimit;
+        }
+        final int queueSize = (int) Math.ceil(estimatedLimit * (1 - minRt / rt));
+
+        double newLimit;
+        // Treat any drop (i.e timeout) as needing to reduce the limit
+
+        double alpha = 3 * Math.log10(estimatedLimit);
+        double beta = 6 * Math.log10(estimatedLimit);
+        double threshold = Math.log10(estimatedLimit);
+
+        // Aggressive increase when no queuing
+        if (queueSize <= threshold) {
+            newLimit = estimatedLimit + beta;
+            // Increase the limit if queue is still manageable
+        } else if (queueSize < alpha) {
+            newLimit = estimatedLimit + Math.log10(estimatedLimit);
+            // Detecting latency so decrease
+        } else if (queueSize > beta) {
+            newLimit = estimatedLimit - Math.log10(estimatedLimit);
+            // We're within he sweet spot so nothing to do
+        } else {
+            return (int) estimatedLimit;
+        }
+
+
+        //newLimit = Math.max(1, Math.min(maxLimit, newLimit));
+        newLimit = (1 - RuleConstant.ADAPTIVE_LIMIT_SMOOTHING) * estimatedLimit + RuleConstant.ADAPTIVE_LIMIT_SMOOTHING * newLimit;
+        return (int) newLimit;
+    }
+}
